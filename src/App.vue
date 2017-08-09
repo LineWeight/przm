e8d9773a07cfc512ca842e4e589537fb07a152ca
Trevor Sargent <tbird1710@gmail.com>
2017-08-08 21:01:14 -0700

    bare bones

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,33 @@
 <template lang="html">
-  <Sidebar></Sidebar>
+  <div class="wrapper">
+
+  <div class="sidebar">
+    <div class="header">
+      <h1>{{title}}</h1>
+      <h2>{{subtitle}}</h2>
+    </div>
+    <div class="buttons">
+      
+    </div>
+  </div>
+  <div class="pens">
+    
+  </div>
+  </div>
 </template>
 
 <script>
 
-import Sidebar from './components/Sidebar.vue'
 
 export default {
   name: 'app',
-  components: {
-    Sidebar
-  },
-  computed: {
-
+  data() {
+    return {
+      title: "Przm",
+      subtitle: "Reference Guide for Prismacolor Premium Art Markers"
+    }
   }
+
 }
 
 </script>
--- src/components/Heading.vue
+++
@@ -1,6 +0,0 @@
-<template>
-	<div class="header">
-		<h1>Przm</h1>
-		<h2>Reference Guide for Prismacolor Premium Art Markers</h2>
-	</div>
-</template>
