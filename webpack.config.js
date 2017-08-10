aacb7d9b99c8e02b9aa889c9fdd65d00d914794f
Trevor Sargent <tbird1710@gmail.com>
2017-08-09 17:54:40 -0700

    webpack stuff

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,63 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
-const webpack = require('webpack'); //to access built-in plugins
-const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin') //installed via npm
+const webpack = require('webpack') //to access built-in plugins
+const path = require('path')
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'build')
-  },
+	entry: './src/index.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'build')
+	},
 
-  module: {
-    rules: [{
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }, {
-        test: /\.(html)$/,
-        use: {
-          loader: 'raw-loader',
-          options: {
-            attrs: [':data-src']
-          }
-        }
-      },
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['env']
-          }
-        }
-      }, {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-        options: {
-          loaders: {}
-          // other vue-loader options go here
-        }
-      },
+	module: {
+		rules: [{
+			test: /\.css$/,
+			use: ['style-loader', 'css-loader']
+		}, {
+			test: /\.(html)$/,
+			use: {
+				loader: 'raw-loader',
+				options: {
+					attrs: [':data-src']
+				}
+			}
+		},
+		{
+			test: /\.js$/,
+			exclude: /(node_modules|bower_components)/,
+			use: {
+				loader: 'babel-loader',
+				options: {
+					presets: ['env']
+				}
+			}
+		}, {
+			test: /\.vue$/,
+			loader: 'vue-loader',
+			options: {
+				loaders: {}
+				// other vue-loader options go here
+			}
+		},
 
-    ]
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './build'
-  },
-  plugins: [
-    // new webpack.optimize.UglifyJsPlugin(),
-    new HtmlWebpackPlugin({
-      template: './index.html',
-      filename: 'index.html',
-      inject: false
-    })
-  ]
+		]
+	},
+	devtool: 'inline-source-map',
+	devServer: {
+		contentBase: './build'
+	},
+	plugins: [
+		// new webpack.optimize.UglifyJsPlugin(),
+		new HtmlWebpackPlugin({
+			template: './index.html',
+			filename: 'index.html',
+			inject: false
+		}),
+		new webpack.DefinePlugin({
+			'process.env': {
+				NODE_ENV: '"production"'
+			}
+		})
+	]
 
-};
+}
