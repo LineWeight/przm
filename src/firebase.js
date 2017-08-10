62cdae24c68246848acab761207f2cc3df77c3f4
Trevor Sargent <tbird1710@gmail.com>
2017-08-09 17:38:09 -0700

    update firebase into for production"

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import Firebase from 'firebase'
 
 const firebaseApp = Firebase.initializeApp({
-	apiKey: 'AIzaSyA0SI2H3Lboh1B1e7hFqGKYo-TfxvH3bY0',
-	authDomain: 'przm-staging.firebaseapp.com',
-	databaseURL: 'https://przm-staging.firebaseio.com',
-	projectId: 'przm-staging',
-	storageBucket: 'przm-staging.appspot.com',
-	messagingSenderId: '338355127780'
+	apiKey: 'AIzaSyCex-hz3fGBVTqlW5ZzcKT25PRjiGFjzDo',
+	authDomain: 'przm-public.firebaseapp.com',
+	databaseURL: 'https://przm-public.firebaseio.com',
+	projectId: 'przm-public',
+	storageBucket: 'przm-public.appspot.com',
+	messagingSenderId: '1049843557333'
 })
 
 export const twitterProvider = new Firebase.auth.TwitterAuthProvider()
